c0adabb899860595d8aaf67534a4a943c4ac1805
SauravSarkar-CodersArcade <ronniesaurav17@gmail.com>
2024-06-01 17:29:39 +0530

    First Commit: Added some java files & made some changes.

---
+++ src/com/basics/Demo.java
@@ -0,0 +1,9 @@
+package com.basics;
+public class Demo {
+    public static void main(String[] args) {
+        String channel = "Coders Arcade";
+        String request = "Subscribe";
+        System.out.println("Please " + request + " to our channel " + channel);
+        System.out.println("Please give the video a thumbs up & hit the bell icon..!");
+    }
+}
